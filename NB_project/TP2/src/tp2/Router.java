82415fd1c7fd32514f1d225b65d98fd26f1f7955
Renato Júnior <renatosergiolopez@hotmail.com>
2018-10-16 16:11:15 -0300

    Adiciona classe da mensagem de trace

--- NB_project/TP2/src/tp2/Router.java
+++ NB_project/TP2/src/tp2/Router.java
@@ -4,7 +4,9 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import org.json.*;
 
 public class Router {
@@ -17,6 +19,11 @@ public class Router {
         um.addDistance("IP 2", 5);
         um.addDistance("IP 3", 10);
         System.out.println(um.getMessageJson());
+        TraceMessage tm = new TraceMessage("Source IP", "Destination IP");
+        tm.addHop("IP 1");
+        tm.addHop("IP 2");
+        tm.addHop("IP 3");
+        System.out.println(tm.getMessageJson());
     }
 
 }
@@ -104,4 +111,39 @@ class UpdateMessage {
         json.put("distances", distances);
         return json.toString();
     }
+}
+
+/**
+ * Classe que representa uma mensagem de trace. Esse tipo de mensagem é descrito
+ * na seção 5.1 da especificação.
+ *
+ * @author renatojuniortmp
+ */
+class TraceMessage {
+
+    private final String source;
+    private final String destination;
+    private List<String> hops;
+
+    private static final String TRACE_MESSAGE_TYPE = "trace";
+
+    public TraceMessage(String s, String d) {
+        this.source = s;
+        this.destination = d;
+        this.hops = new ArrayList<>();
+    }
+
+    public void addHop(String ip) {
+        this.hops.add(ip);
+    }
+
+    public String getMessageJson() {
+        JSONObject json = new JSONObject();
+        json.put("type", TRACE_MESSAGE_TYPE);
+        json.put("source", this.source);
+        json.put("destination", this.destination);
+        json.put("hops", hops);
+        return json.toString();
+    }
+
 }
