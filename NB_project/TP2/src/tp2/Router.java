d46762245910864e6b029610bc05d71a7abff351
Ykaro237 <tiagonegri237@gmail.com>
2018-10-22 14:41:04 -0300

    an example test in main to see that works!

--- NB_project/TP2/src/tp2/Router.java
+++ NB_project/TP2/src/tp2/Router.java
@@ -1,9 +1,19 @@
 package tp2;
+import java.util.ArrayList;
 
 public class Router {
-
     public static void main(String[] args) {
-
+        ArrayList<RouterRIP> routers = new ArrayList<RouterRIP>();
+        routers.add(new RouterRIP("127.0.0.1",10));
+        routers.add(new RouterRIP("127.0.0.2",10));
+        routers.add(new RouterRIP("127.0.0.3",10));
+        
+        routers.get(0).addNewRoute("127.0.0.3","127.0.0.2",4);
+        routers.get(1).addNewRoute("127.0.0.3","127.0.0.3",3);
+        DataMessage dataMessage = new DataMessage("127.0.0.1","127.0.0.3","OLA!");
+        routers.get(0).sendMessage(dataMessage,"127.0.0.2");
+        routers.get(1).receiveMessage();
+        routers.get(2).receiveMessage();
     }
 
 }
