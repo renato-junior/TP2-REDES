d800916c04a48f030146ce2e134db69d2d813e5d
Ykaro237 <tiagonegri237@gmail.com>
2018-10-29 11:37:08 -0300

    read the input on a thread now

--- NB_project/TP2/src/tp2/Router.java
+++ NB_project/TP2/src/tp2/Router.java
@@ -7,22 +7,27 @@ public class Router{
     public static void main(String[] args) {
         RouterRIP mainRouter = new RouterRIP(args[0],Integer.parseInt(args[1]));
         Timmer timmer = new Timmer();
+        InputHandler input = new InputHandler();
         
         timmer.start();
         mainRouter.start();
+        input.start();
         while(true){
             if(timmer.timePassed >= 4*mainRouter.getPeriod()){
                 mainRouter.sendUpdateMessages();
                 timmer.reset();
             }
-            Scanner inpt = new Scanner(System.in);
-            String inputLine = inpt.nextLine();            
+            
+            String inputLine = input.getNextCommand();
+            if(inputLine == null)
+                continue;
             String command = inputLine.split(" ")[0];
             String firstArg = inputLine.split(" ")[1];
             String secondArg;
             if(command.equals("add")){
                 secondArg = inputLine.split(" ")[2];
                 mainRouter.addNewRoute(firstArg, firstArg, Integer.parseInt(secondArg));
+                mainRouter.sendUpdateMessages();
             }
             else if(command.equals("del")){
                 mainRouter.deleteRoute(firstArg);
@@ -37,6 +42,29 @@ public class Router{
             }
         }
     }
+}
+
+class InputHandler extends Thread{
+    ArrayList<String> inputs;    
+    InputHandler(){
+        inputs = new ArrayList<String>();
+    }
+    
+    public void run(){
+        while(true){
+            Scanner inpt = new Scanner(System.in);
+            inputs.add(inpt.nextLine());
+        }
+    }
+    
+    public String getNextCommand(){
+        if(this.inputs.size() == 0)
+            return null;
+        else{
+            return this.inputs.remove(0);
+        }
+    }
+
 }
 
 class Timmer extends Thread{
